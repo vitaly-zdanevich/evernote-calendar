e5123007514fe347ddd69d93d4f24aa16dfb7a6d
Vitaly Zdanevich <zdanevich.vitaly@ya.ru>
2015-12-16 04:39:27 +0300

    calendar shows onclick

--- evernote-calendar.user.js
+++ evernote-calendar.user.js
@@ -1,1 +1,29 @@
-/home/vitaly/.mozilla/firefox/3gc290r0.default/gm_scripts/evernote-calendar/evernote-calendar.user.js
+// ==UserScript==
+// @name        evernote-calendar
+// @namespace   zdanevich.vitaly@ya.ru
+// @match       https://sandbox.evernote.com/Home.action*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+function addButton() {
+    // replace user's avatar with calendar button
+    var date = new Date().getDate();
+    $('.GNTMVRYDXB')[0].innerHTML = '<div id="calendar-button">' + date + '</div>';
+}
+setTimeout(function(){
+  addButton();
+  $('#calendar-button').click(function() {
+    showCalendar();
+  });
+  $('.gwt-DatePicker').clone().appendTo('html');
+  $('.gwt-DatePicker')[1].id = 'calendar';
+}, 10000);
+
+function showCalendar() {
+  $('#calendar').css({position:'absolute', top:0, background:'#000', 'z-index':999, height:'100%'});
+  $('#calendar').find('.datePickerDays').css({width:'100%', height:'100%'});
+  $('#calendar').find('colgroup').css({height:'100%'});
+}
+
+// .datePickerDay: increase width & height to fullscreen
